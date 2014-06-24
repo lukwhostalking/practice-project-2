ca1cb71ce17b7b8a889cc18f014d62127511fabe
ANUPRIYA AGRAWAL <anupriyaagrawal@ANUPRIYAs-MacBook-Air.local>
2014-06-24 17:21:46 -0400

    Initial Commit

---
+++ practice-project-2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  practice-project-2
+//
+//  Created by ANUPRIYA AGRAWAL on 6/24/14.
+//  Copyright (c) 2014 IshanBhalla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ practice-project-2/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  practice-project-2
+//
+//  Created by ANUPRIYA AGRAWAL on 6/24/14.
+//  Copyright (c) 2014 IshanBhalla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
